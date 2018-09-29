31c2ad3d8864eefd92345667747b92f0d8e5e689
kkyoon86 <atunelf1@naver.com>
2018-09-29 17:47:07 +0900

    퍼블리싱 수정

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
   setNaniLinkAnimation();
   multipleToggle();
   langSelect();
-
-
+  progressBarSe();
+  downloadSelect();
+  closeAllSelect(elmnt);
 
 });
 
@@ -135,15 +136,7 @@ $(function () {
       scale: 1.1
     }) // the tween durtion can be omitted and defaults to 1
     .addTo(controller);
-  var scene = new ScrollMagic.Scene({
-      triggerElement: "#mockupTrigger",
-      duration: 400
-    })
-    // animate color and top border in relation to scroll position
-    .setTween("#mockupEffect2", {
-      scale: 1.1
-    }) // the tween durtion can be omitted and defaults to 1
-    .addTo(controller);
+
 
   // 중간 버튼 패럴랙스
 
@@ -499,7 +492,7 @@ $(function () {
   time = setInterval(timerCountdown, 1000);
 });
 
-
+/* -------------------------------------------------------------------------------- */
 //프로그래스 바
 
 'use strict';
@@ -522,8 +515,41 @@ let percentageInterval = setInterval(() => {
 }, 50);
 
 
-function langSelect() {
 
+
+/* -------------------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------------- */
+//프로그래스 바
+function progressBarSe() {
+  'use strict';
+
+  let advanceProgress = (amount) => {
+    amount = amount || 0;
+    document.getElementById('progressCountSe').innerHTML = amount;
+    document.getElementById('progressBarSe').style.backgroundImage = 'linear-gradient(110deg, #ffffff ' + amount + '%, rgba( 255, 255, 255, 0.2 ) ' + amount + '%)';
+    document.getElementById('progressTextSe').style.backgroundImage = 'linear-gradient(110deg, rgba( 0, 0, 0, 0.2 ) ' + amount + '%, rgba( 0, 0, 0, 0 ) ' + amount + '%)';
+  }
+
+  let percentage = 0;
+  let percentageInterval = setInterval(() => {
+    percentage++;
+    if (percentage <= 70) {
+      advanceProgress(percentage);
+    } else {
+      clearInterval(percentageInterval);
+    }
+  }, 50);
+
+}
+
+
+/* -------------------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------------- */
+function langSelect() {
   //언어 변환기
   document.getElementById("langSelect").onchange = function () {
     if (this.selectedIndex !== 0) {
@@ -532,3 +558,100 @@ function langSelect() {
   };
 
 }
+
+/* -------------------------------------------------------------------------------- */
+
+
+
+
+
+
+/* -------------------------------------------------------------------------------- */
+
+
+
+//화이트페이퍼 다운로드 셀렉터
+
+function downloadSelect() {
+  var x, i, j, selElmnt, a, b, c;
+  /*look for any elements with the class "custom-select":*/
+  x = document.getElementsByClassName("custom-select");
+  for (i = 0; i < x.length; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 0; j < selElmnt.length; j++) {
+      /*for each option in the original select element,
+      create a new DIV that will act as an option item:*/
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function (e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
+}
+
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+
+
+
+
+
+
+
+/* -------------------------------------------------------------------------------- */
